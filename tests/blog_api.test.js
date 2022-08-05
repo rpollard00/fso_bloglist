0f75a5de343c61606fa8606d25587b96faf50e35
Reese Pollard <rpollard@gmail.com>
2022-08-04 21:15:41 -0700

    Implement 4.8

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,3 +4,18 @@ const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('get blogs returns the correct amount', async () => {
+  const response = await api.get('/api/blogs')
+  
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -44,3 +44,8 @@ const blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map(blog => blog.toJSON())
 }
+
+module.exports = {
+  initialBlogs,
+  blogsInDb
+}
