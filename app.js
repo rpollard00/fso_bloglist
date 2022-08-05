8bfc5a2f7959edd5cef85bdd2c2b2afa43a12cc2
Reese Pollard <rpollard@gmail.com>
2022-08-05 16:14:49 -0700

    Implement 4.15

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 
 const blogRouter = require("./controllers/blogs")
+const usersRouter = require("./controllers/users")
 
 logger.info(config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
@@ -14,5 +15,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogRouter)
+app.use("/api/users", usersRouter)
 
 module.exports = app
