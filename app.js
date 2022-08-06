e55b93a7d7e5b8ed8eaa36b1937a0459c427d0d3
Reese Pollard <rpollard@gmail.com>
2022-08-06 12:11:26 -0700

    4.20 - Refactor token helper to middleware

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use("/api/login", loginRouter)
+app.use(middleware.getTokenFrom)
 app.use("/api/blogs", blogRouter)
 app.use("/api/users", usersRouter)
-app.use("/api/login", loginRouter)
+
+
 
 app.use(middleware.errorHandler)
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -17,6 +17,17 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const getTokenFrom = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    request["token"] = authorization.substring(7)
+  }
+
+  next()
+}
+
 module.exports = {
-  errorHandler
+  errorHandler,
+  getTokenFrom
 }
