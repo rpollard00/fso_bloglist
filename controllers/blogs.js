1531379fbd82eff759d80911888771f8e431ede8
Reese Pollard <rpollard@gmail.com>
2022-08-25 15:11:43 -0700

    Add backend for comments

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,13 @@
 const blogRouter = require("express").Router()
 const Blog = require("../models/blog")
+const Comment = require("../models/comment")
 const jwt = require('jsonwebtoken')
+const logger = require('../utils/logger')
 
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
+  const blogs = await Blog.find({})
+    .populate('user', { username: 1, name: 1, id: 1 })
+    .populate('comments', { content: 1, id: 1 })
   response.json(blogs)
 })
 
@@ -47,10 +51,32 @@ blogRouter.put("/:id", async (request, response) => {
     url: body.url,
     author: body.author,
     user: body.user.id,
+    comments: body.comments.id
   }
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
   response.status(200).json(updatedBlog) 
+})
+
+blogRouter.post("/:id/comments", async (request, response) => {
+  const body = request.body
+  const comment = new Comment({
+    content: body.content
+  })
+  logger.info("Please work pleaase")
+  const newComment = await comment.save()
+  console.log("newComment", newComment)
+  // find the blog
+  const blog = await Blog.findById(request.params.id)
+  // update the blog's comment array with the new comment id
+  blog.comments = blog.comments.concat(newComment._id) || [newComment._id]
+  await blog.save()
+  //const updatedBlog = { ...blog, comments: blog.comments.concat(newComment._id) || [newComment._id]}
+  //console.log("updatedBlog with comment", updatedBlog)
+  // save the blog to the database
+  //await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
+  // return the comment
+  response.status(201).json(newComment)
 })
 
 blogRouter.delete("/:id", async (request, response) => {
--- models/blog.js
+++ models/blog.js
@@ -11,7 +11,11 @@ const blogSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
-  }
+  },
+  comments: [ { // reference to blog Objects
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  } ]
 })
 
 blogSchema.set("toJSON", {
